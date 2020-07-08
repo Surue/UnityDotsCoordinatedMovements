e269b15af10d88206181ca482964f7bba3f8308f
surue <nicolas06062000@hotmail.com>
2020-07-08 16:31:25 +0200

    Agents follow simple path

---
+++ Assets/Scripts/AI/Navigation/PathFollowSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+public class PathFollowSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        //Dynamic buffer must be at the start
+        Entities.ForEach((DynamicBuffer<PathPositions> pathPositionBuffer, ref PathFollow pathFollow, ref Translation position, ref PhysicsVelocity velocity) =>
+        {
+            if (pathFollow.pathPositionIndex >= 0)
+            {    
+                float3 targetPosition = new float3(pathPositionBuffer[pathFollow.pathPositionIndex].position.x, 0, pathPositionBuffer[pathFollow.pathPositionIndex].position.y);
+                
+                float3 direction = math.normalizesafe(targetPosition - position.Value);
+                float moveSpeed = 3;
+                
+                velocity.Linear = direction * moveSpeed;
+                
+                if (math.distance(new float2(position.Value.x, position.Value.z), pathPositionBuffer[pathFollow.pathPositionIndex].position) < 0.1f)
+                {
+                    pathFollow.pathPositionIndex--;
+                }
+            }
+        });
+    }
+}
--- Assets/Scripts/AI/Pathfinding/PathFinderSystem.cs
+++ Assets/Scripts/AI/Pathfinding/PathFinderSystem.cs
@@ -4,12 +4,14 @@ public class PathFinderSystem : ComponentSystem
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((Entity entity, DynamicBuffer<PathPositions> pathPositionBuffer, ref PathFindingRequest request) =>
+        Entities.ForEach((Entity entity, DynamicBuffer<PathPositions> pathPositionBuffer, ref PathFindingRequest request, ref PathFollow pathFollow) =>
         {
             //TODO call pathfinding job here
             pathPositionBuffer.Clear();
-            pathPositionBuffer.Add(new PathPositions{ position = request.startPos});
             pathPositionBuffer.Add(new PathPositions{ position = request.endPos});
+            pathPositionBuffer.Add(new PathPositions{ position = request.startPos});
+
+            pathFollow.pathPositionIndex = 1;
             
             //Remove path finding request from the entity
             PostUpdateCommands.RemoveComponent<PathFindingRequest>(entity);
